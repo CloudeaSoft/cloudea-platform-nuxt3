e55187882d1b7847db2b6ba1db311ad9b6fa62a6
Cloudea <2025140062@qq.com>
2024-04-17 20:10:11 +0800

    2024-04-17. Add user page frame.

--- components/user/Content.vue
+++ components/user/Content.vue
@@ -8,21 +8,28 @@ const profile = ref<UserProfile>(response.value?.Data!)
 </script>
 
 <template>
-  <div class="user-container cloudea-area">
+  <div class="user-container">
     <div class="user-content">
-      <div class="user-header">
-        <ul>
-          <div class="title">{{ userId }}</div>
-          <li>{{ profile.DisplayName }}</li>
-          <li>{{ profile.UserName }}</li>
-          <li>{{ profile.Leaves }}</li>
-          <li>{{ profile.Signature }}</li>
-          <li>{{ profile.AvatarUrl }}</li>
-        </ul>
+      <div class="user-header cloudea-area">
+        <div class="avatar-area">
+          <NuxtImg class="avatar" :src="profile.AvatarUrl"></NuxtImg>
+        </div>
+        <div class="info-area">
+          <div class="name-area">
+            <span class="display-name">{{ profile.DisplayName }}</span>
+            <span class="user-name">@{{ profile.UserName }}</span>
+          </div>
+          <div class="leaves">
+            <Icon name="lucide:clover" />
+            <span class="count">{{ profile.Leaves }}</span>
+          </div>
+          <div class="signature">{{ profile.Signature }}</div>
+        </div>
       </div>
       <div class="user-body">
-        <div class="user-nav"></div>
-        <div class="user-slot">
+        <div class="user-board cloudea-area">留言板</div>
+        <div class="user-main cloudea-area">
+          <UserNav></UserNav>
           <slot />
         </div>
       </div>
@@ -39,8 +46,90 @@ const profile = ref<UserProfile>(response.value?.Data!)
   overflow: hidden;
 
   .user-content {
+    display: flex;
+    flex-direction: column;
     height: 100%;
     width: 800px;
+
+    .user-header {
+      display: flex;
+      height: 300px;
+
+      .avatar-area {
+        width: 250px;
+        display: flex;
+
+        .avatar {
+          margin: auto;
+          width: 150px;
+          height: 150px;
+          border-radius: 75px;
+        }
+      }
+
+      .info-area {
+        flex: 1;
+        display: flex;
+        padding-top: 80px;
+        flex-direction: column;
+
+        .name-area {
+          display: flex;
+          align-items: flex-end;
+
+          .display-name {
+            font-weight: 700;
+            font-size: 2rem;
+          }
+
+          .user-name {
+            margin-left: 10px;
+            padding-bottom: 2px;
+            font-size: 1.5rem;
+            color: var(--cloudea-font-color-0);
+          }
+        }
+
+        .leaves {
+          margin-top: 30px;
+          font-size: 1.5rem;
+
+          & svg {
+            color: var(--cloudea-green-3);
+          }
+
+          .count {
+            margin-left: 8px;
+            color: var(--cloudea-red-2);
+          }
+        }
+
+        .signature {
+          margin-top: 30px;
+          font-size: 1.5rem;
+          font-style: italic;
+        }
+      }
+    }
+
+    .user-body {
+      margin-top: 30px;
+      display: flex;
+
+      .user-board {
+        width: 250px;
+        height: 600px;
+        padding: 20px;
+      }
+
+      .user-main {
+        margin-left: 30px;
+        flex: 1;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+      }
+    }
   }
 }
 </style>
---
+++ components/user/Nav.vue
@@ -0,0 +1,1 @@
+<template><div class="">nav</div></template>
