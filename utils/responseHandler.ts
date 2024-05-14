7f74a50d9068f4b0e0ea5206e2c84f0b09de6549
Cloudea <2025140062@qq.com>
2024-05-14 10:51:36 +0800

    2024-05-14: Fix responseHandler

--- utils/responseHandler.ts
+++ utils/responseHandler.ts
@@ -20,52 +20,52 @@ type OnResponseErrorContext<R extends ResponseType = 'json'> = FetchContext & {
 const handleStatus = (status: number) => {
   switch (status) {
     case 200:
-      useMessage('200 Request successful', 'success', 3000)
+      console.log('200 Request successful', 'success', 3000)
       break
     case 201:
-      useMessage('201 Resource created', 'success', 3000)
+      console.log('201 Resource created', 'success', 3000)
       break
     case 204:
-      useMessage('204 No content', 'info', 3000)
+      console.log('204 No content', 'info', 3000)
       break
     case 301:
-      useMessage('301 Moved Permanently', 'info', 3000)
+      console.log('301 Moved Permanently', 'info', 3000)
       break
     case 304:
-      useMessage('304 Not Modified', 'info', 3000)
+      console.log('304 Not Modified', 'info', 3000)
       break
     case 400:
-      useMessage('400 Bad Request', 'error', 3000)
+      console.log('400 Bad Request', 'error', 3000)
       break
     case 401:
-      useMessage('401 Unauthorized', 'error', 3000)
+      console.log('401 Unauthorized', 'error', 3000)
       useUserStore().removeToken()
       break
     case 403:
-      useMessage('403 Forbidden', 'error', 3000)
+      console.log('403 Forbidden', 'error', 3000)
       break
     case 404:
-      useMessage('404 Not Found', 'error', 3000)
+      console.log('404 Not Found', 'error', 3000)
       break
     case 409:
-      useMessage('409 Conflict', 'error', 3000)
-      useUserStore().removeToken()
+      console.log('409 Conflict', 'error', 3000)
+      useUserStore().$reset()
       navigateTo(useNuxtApp().$localePath('/login'))
       break
     case 500:
-      useMessage('500 Internal Server Error', 'error', 3000)
+      console.log('500 Internal Server Error', 'error', 3000)
       break
     case 502:
-      useMessage('502 Bad Gateway', 'error', 3000)
+      console.log('502 Bad Gateway', 'error', 3000)
       break
     case 503:
-      useMessage('Service Unavailable', 'error', 3000)
+      console.log('Service Unavailable', 'error', 3000)
       break
     case 504:
-      useMessage('Gateway Timeout', 'error', 3000)
+      console.log('Gateway Timeout', 'error', 3000)
       break
     default:
-      useMessage('Unknown Server Error', 'error', 3000)
+      console.log('Unknown Server Error', 'error', 3000)
       break
   }
 }
@@ -75,7 +75,10 @@ export const onResponse = (context: OnResponseContext) => {
 }
 
 const onResponseError = (context: OnResponseErrorContext) => {
-  console.error('Response error:', context.response)
+  if (context.response.status === 409) {
+    useUserStore().$reset()
+    navigateTo(useNuxtApp().$localePath('/login'))
+  }
 }
 
 export const responseHandler = { onResponse, onResponseError }
